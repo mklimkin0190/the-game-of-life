560122b5c53f6eb94101d805321b16943432d0cf
Maria Klimkina <0910.nanami@gmail.com>
2014-08-30 19:37:42 +0300

    meaningful cells and a toad

--- blinker.py
+++ blinker.py
@@ -1,5 +1,7 @@
+global size
+size = 6
+
 def draw_cells(cells):
-	size = 5
 	print '---------'
 	for i in range(0, size):
 		for j in range(0, size):
@@ -28,11 +30,16 @@ def cell_is_reborn(coords, cells):
 	else:
 		return False
 
+def meaningfull_cells(cells):
+	xlist = list(cell[0] for cell in cells)
+	ylist = list(cell[1] for cell in cells)
+	return (range(min(xlist) - 1, max(xlist) + 2), range(min(ylist) - 1, max(ylist) + 2))
+
 def round(cells):
-	size = 5
 	end_state = []
-	for i in range(0, size):
-		for j in range(0, size):
+	xboundaries, yboundaries = meaningfull_cells(cells)
+	for i in xboundaries:
+		for j in yboundaries:
 			if (i, j) in cells and not cell_dies((i, j), cells):
 				end_state.append((i, j))
 			elif (i, j) not in cells and cell_is_reborn((i, j), cells):
@@ -41,7 +48,8 @@ def round(cells):
 
 def test():
 	seed = [(0, 0), (0, 1), (1, 0), (1, 1)]
-	size = 5
+	blinker = [(2, 1), (2, 2), (2, 3)]
+	toad = [(2, 2), (2, 3), (2, 4), (3, 1), (3, 2), (3, 3)]
 	draw_cells(seed)
 	for cell in seed:
 		assert neighbour_count(cell, seed) == 3
@@ -51,9 +59,8 @@ def test():
 			assert (i, j) in seed or not cell_is_reborn((i, j), seed)
 
 	assert round(seed) == seed
-
-	blinker = [(2, 1), (2, 2), (2, 3)]
 	assert round(round(blinker)) == blinker
+	assert round(round(toad)) == toad
 
 test()
 
