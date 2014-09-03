389101f5f8f50a7eb3c05c0926ac37dc64c8265c
Maria Klimkina <0910.nanami@gmail.com>
2014-09-03 20:52:34 +0300

    refactor

--- blinker.py
+++ blinker.py
@@ -11,18 +11,19 @@ def draw_cells(cells):
 	print '---------'
 
 def neighbour_count(cell, cells):
+	x, y = cell
 	neighbour_count = 0
-	for i in range(-1, 2):
-		for j in range(-1, 2):
-			if (cell[0] + i, cell[1] + j) in cells and not (i == 0 and j == 0):
+	for i in range(x - 1, x + 2):
+		for j in range(y - 1, y + 2):
+			if (i, j) in cells and not (i == x and j == y):
 				neighbour_count += 1
 	return neighbour_count
 
-def cell_dies(cell, cells):
-	return neighbour_count(cell, cells) < 2 or neighbour_count(cell, cells) > 3
+def cell_dies(neighbour_count):
+	return neighbour_count < 2 or neighbour_count > 3
 
-def cell_is_reborn(coords, cells):
-	return neighbour_count(coords, cells) == 3
+def cell_is_reborn(neighbour_count):
+	return neighbour_count == 3
 
 def meaningfull_cells(cells):
 	xlist = list(cell[0] for cell in cells)
@@ -34,9 +35,11 @@ def round(cells):
 	xboundaries, yboundaries = meaningfull_cells(cells)
 	for i in xboundaries:
 		for j in yboundaries:
-			if (i, j) in cells and not cell_dies((i, j), cells):
-				end_state.append((i, j))
-			elif (i, j) not in cells and cell_is_reborn((i, j), cells):
+			count = neighbour_count((i, j), cells)
+			if (i, j) in cells:
+				if not cell_dies(count):
+					end_state.append((i, j))
+			elif cell_is_reborn(count):
 				end_state.append((i, j))
 	return end_state
 
@@ -46,11 +49,13 @@ def test():
 	toad = [(2, 2), (2, 3), (2, 4), (3, 1), (3, 2), (3, 3)]
 	draw_cells(seed)
 	for cell in seed:
-		assert neighbour_count(cell, seed) == 3
-		assert not cell_dies(cell, seed)
+		count = neighbour_count(cell,            seed)
+		assert count == 3
+		assert not cell_dies(count)
 	for i in range(0, size):
 		for j in range(0, size):
-			assert (i, j) in seed or not cell_is_reborn((i, j), seed)
+			count = neighbour_count((i, j), seed)
+			assert (i, j) in seed or not cell_is_reborn(count)
 
 	assert round(seed) == seed
 	assert round(round(blinker)) == blinker
